e4c925209933150954eeb74d634b06cfbb5e2cc6
Pedro Rodrigues <pedro.rodrigues@josapar.com.br>
2026-03-27 09:03:01 -0300

    Ajuste do scraping

--- infra/external/fundamentus/fundamentus_client.py
+++ infra/external/fundamentus/fundamentus_client.py
@@ -3,23 +3,34 @@ import pandas as pd
 import time
 
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class FundamentusClient:
     URL = "https://www.fundamentus.com.br/fii_resultado.php"
 
     def fetch_fiis(self) -> pd.DataFrame:
         options = uc.ChromeOptions()
-        options.headless = True  # pode testar False se quiser ver
+        options.headless = False  # pode testar False se quiser ver
 
-        driver = uc.Chrome(options=options)
-
+        driver = uc.Chrome(version_main=146, options=options)
         driver.get(self.URL)
-
-        time.sleep(5)  # espera carregar Cloudflare
+        
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//table//a"))
+        )
+        
+        time.sleep(3)
 
         html = driver.page_source
 
-        driver.quit()
-
+        try:
+            driver.quit()
+        except Exception as e:
+            print(f"Erro ao fechar o navegador: {e}")
+            pass
+    
         df = pd.read_html(html)[0]
         df.columns = [col.strip() for col in df.columns]
 
