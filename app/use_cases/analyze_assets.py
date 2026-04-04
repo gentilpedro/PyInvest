6b3a21cbc96e70041b7cb4b4cae35aee91b4c5da
gentilpedro <gentil.pedro21@gmail.com>
2026-04-04 12:35:43 -0300

    Feito a conclusao do webscrap da pagina

--- app/use_cases/analyze_assets.py
+++ app/use_cases/analyze_assets.py
@@ -1,5 +1,6 @@
 from infra.external.fundamentus.fundamentus_client import FundamentusClient
 from infra.writers.excel_writer import ExcelWriter
+#from app.domain.filters.fii_filter import FiiFilter
 
 
 class AnalyzeAssetsUseCase:
@@ -17,5 +18,15 @@ class AnalyzeAssetsUseCase:
         print("💾 Salvando Excel...")
 
         self.writer.save(df, "fiis.xlsx")
-
         print("✅ Finalizado!")
+
+        # filter_engine = FiiFilter()
+        #
+        # filters = {
+        #     "dy_min": 8,
+        #     "pvp_max": 1.0,
+        #     "liquidez_min": 100000
+        # }
+        #
+        # df = filter_engine.apply(df, filters)
+        # df = filter_engine.rank(df)
